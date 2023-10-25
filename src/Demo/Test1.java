ee94a557916a1ca736ec1117d11e80383b161f3d
SinghDinesh <SinghDinesh@User>
2023-10-25 16:10:07 +0530

    Commit.....

---
+++ src/Demo/Test1.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String str="Dinesh Kumar";
+		
+		if(str=="Dinesh Kumar") {
+			System.out.println("it is my name");
+		}
+		else {
+			System.out.println("it is not my name");
+		}
+	}
+
+}
